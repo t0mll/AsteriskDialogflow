58d351db867535f3fe5e5ed2fe4d96a5a4976ff0
Thomas Carlier <carlier.thomas@gmail.com>
2020-10-10 21:45:05 +1000

    Change worker to create a bridge instead of playing a soundfile

--- AsteriskDialogflow.AriBridge/Worker.cs
+++ AsteriskDialogflow.AriBridge/Worker.cs
@@ -14,12 +14,17 @@ namespace AsteriskDialogflow.AriBridge
     {
         private readonly ILogger<Worker> _logger;
         private readonly AriClient _actionClient;
+        private readonly Bridge _bridge;
+        private const string APP_NAME = "bridge_test";
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
             _actionClient = new AriClient(new StasisEndpoint("127.0.0.1", 8088, "asterisk", "asterisk"),
-                    "HelloWorld");
+                    "Bridge");
+
+            // Create simple bridge
+            _bridge = _actionClient.Bridges.Create("mixing", Guid.NewGuid().ToString(), APP_NAME);
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -28,9 +33,7 @@ namespace AsteriskDialogflow.AriBridge
 
             // Hook into required events
             _actionClient.OnStasisStartEvent += c_OnStasisStartEvent;
-            _actionClient.OnChannelDtmfReceivedEvent += ActionClientOnChannelDtmfReceivedEvent;
-            _actionClient.OnConnectionStateChanged += ActionClientOnConnectionStateChanged;
-
+            _actionClient.OnStasisEndEvent += c_OnStasisEndEvent;
             _actionClient.Connect();
 
             return Task.CompletedTask;
@@ -39,35 +42,25 @@ namespace AsteriskDialogflow.AriBridge
         private void ActionClientOnConnectionStateChanged(object sender)
         {
             _logger.LogDebug("Connection state is now {0}", _actionClient.Connected);
-        }
-
-        private void ActionClientOnChannelDtmfReceivedEvent(IAriClient sender, ChannelDtmfReceivedEvent e)
-        {
-            _logger.LogDebug($"Received DTMF {e.Digit}");
-
-            // When DTMF received
-            switch (e.Digit)
-            {
-                case "*":
-                    sender.Channels.Play(e.Channel.Id, "sound:asterisk-friend");
-                    break;
-                case "#":
-                    sender.Channels.Play(e.Channel.Id, "sound:goodbye");
-                    sender.Channels.Hangup(e.Channel.Id, "normal");
-                    break;
-                default:
-                    sender.Channels.Play(e.Channel.Id, string.Format("sound:digits/{0}", e.Digit));
-                    break;
-            }
+            _actionClient.Applications.Subscribe(APP_NAME, $"bridge:{_bridge.Id}");
         }
 
         private void c_OnStasisStartEvent(IAriClient sender, StasisStartEvent e)
         {
-            // Answer the channel
-            sender.Channels.Answer(e.Channel.Id);
+            _logger.LogDebug($"Answer channel - {e.Channel.Id}");
+            _actionClient.Channels.Answer(e.Channel.Id);
 
-            // Play an announcement
-            sender.Channels.Play(e.Channel.Id, "sound:hello-world");
+            _logger.LogDebug($"Add channel {e.Channel.Id} to Bridge {_bridge.Id}");
+            _actionClient.Bridges.AddChannelAsync(_bridge.Id, e.Channel.Id, "member");
+        }
+
+        private void c_OnStasisEndEvent(IAriClient sender, StasisEndEvent e)
+        {
+            _logger.LogDebug($"Remove channel {e.Channel.Id} from Bridge {_bridge.Id}");
+            _actionClient.Bridges.RemoveChannelAsync(_bridge.Id, e.Channel.Id);
+
+            _logger.LogDebug($"Hangup channel - {e.Channel.Id}");
+            _actionClient.Channels.Hangup(e.Channel.Id, "normal");
         }
     }
 }
